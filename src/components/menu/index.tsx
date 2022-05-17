2146023c617b5e0321e0404d57b1c2ed862c32db
luccasoncini <luccadiassoncini@gmail.com>
2022-05-17 09:18:29 -0300

    fix errors in menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -6,18 +6,21 @@ export function MenuNavigation() {
     const { isMenuOpened, handleToggleMenu } = useMenu();
 
     return(
-        <nav className={ isMenuOpened ? 'navigation opened' : 'navigation'}>
-            <ul>
-                <li><a className="active" onClick={handleToggleMenu} href="">Home</a></li>
-                <li><a className="active" onClick={handleToggleMenu} href="">Sobre Mim</a></li>
-                <li><a className="active" onClick={handleToggleMenu} href="">Habilidades</a></li>
-                <li><a className="active" onClick={handleToggleMenu} href="">Trabalhos</a></li>
-                <li><a className="active" onClick={handleToggleMenu} href="">Contatos</a></li>
-            </ul>
-            <div className="redes-sociais">
-                <a href="https://www.linkedin.com/in/lucca-soncini-727930207/" target="_blank"><FaLinkedinIn /></a>
-                <a href="https://github.com/Luccasoncini" target="_blank"><FaGithub /></a>
-            </div>
-        </nav>
+        <div className={ isMenuOpened ? 'navigation opened' : 'navigation'}>
+            <nav className="menu">
+                <ul>
+                    <li><a className="active" onClick={handleToggleMenu} href="">Home</a></li>
+                    <li><a className="active" onClick={handleToggleMenu} href="">Sobre Mim</a></li>
+                    <li><a className="active" onClick={handleToggleMenu} href="">Habilidades</a></li>
+                    <li><a className="active" onClick={handleToggleMenu} href="">Trabalhos</a></li>
+                    <li><a className="active" onClick={handleToggleMenu} href="">Contatos</a></li>
+                </ul>
+                <div className="redes-sociais">
+                    <a href="https://www.linkedin.com/in/lucca-soncini-727930207/" target="_blank"><FaLinkedinIn /></a>
+                    <a href="https://github.com/Luccasoncini" target="_blank"><FaGithub /></a>
+                </div>
+            </nav>
+        </div>
+        
     )
 } 
