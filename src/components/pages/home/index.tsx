f0c3041e6db70c124e781eeae29b320f9aec8240
luccasoncini <luccadiassoncini@gmail.com>
2022-05-20 20:57:25 -0300

    adicionando o react-toastify e otimizando nosso formulario

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,17 +1,25 @@
+import { ToastContainer } from 'react-toastify';
 import { AboutSection } from '../../sections/about';
 import { ContactFormSection } from '../../sections/contactForm';
 import { IntroductionSection } from '../../sections/introduction';
 import { SkillsSection } from '../../sections/skills';
 import { WorksSection } from '../../sections/works';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export function Home() {
     return(
         <>
-            <IntroductionSection/>
-            <AboutSection/>
-            <SkillsSection/>
-            <WorksSection/>
-            <ContactFormSection/>
+            <IntroductionSection />
+            <AboutSection />
+            <SkillsSection />
+            <WorksSection />
+            <ContactFormSection />
+
+            <ToastContainer 
+            autoClose={3000}
+            hideProgressBar={true}
+            />
         </>
     )
 }
--- src/components/sections/contactForm/index.tsx
+++ src/components/sections/contactForm/index.tsx
@@ -1,20 +1,53 @@
 import './contactForm.scss'
 import emailjs from 'emailjs-com'
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify';
 
 export function ContactFormSection() {
 
-    function sendEmail(e: FormEvent) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [isSendingForm, setIsSendingForm] = useState(false);
+
+    function HandleSetName(event: React.FormEvent<HTMLInputElement>) {
+        setName(event.currentTarget.value);
+        console.log(name);
+    }
+    function HandleSetEmail(event: React.FormEvent<HTMLInputElement>) {
+        setEmail(event.currentTarget.value)
+        console.log(email);
+    }
+    function HandleSetMessage(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setMessage(event.currentTarget.value);
+        console.log(message);
+    }
+
+
+    async function sendEmail(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
 
-        emailjs.sendForm(
+        setIsSendingForm(true);
+
+        await emailjs.sendForm(
             'service_shfcggk',
             'template_ga97qzp',
             '#formulario',
             'zNLMkYLRyXR0tJ2LI'
-        ).then(res=>{
-            console.log(res)
-        }).catch(err => console.log(err));
+        )
+        .then(res=>{
+            setName("");
+            setEmail("");
+            setMessage("");
+            toast.success("Seu formulário foi enviado!");
+        })
+        .catch(err => {
+            toast.error("Erro ao enviar o formulário, tente novamente mais tarde");
+        });
+
+        setIsSendingForm(false);
+
+        
     }
 
     return(
@@ -28,15 +61,39 @@ export function ContactFormSection() {
                                 <p>Deseja mandar uma ideia ou talvez uma oportunidade de trabalho? fique a vontade para mandar pelo formulário, todos os e-mails enviados irão chegar a mim, e o mais rápido possivel irei te responder</p>
                             </div>
                             <form action="" id="formulario" onSubmit={sendEmail}>
-                                <input required type="text" name="from_name" placeholder="Nome"/>
-                                <input required type="email" name="email" placeholder="Email"/>
-                                <textarea required name="message" placeholder="Comentário"></textarea>
-                                <button type="submit">Enviar</button>
+                                <input 
+                                    value={name} 
+                                    onChange={HandleSetName} 
+                                    required 
+                                    type="text" 
+                                    name="from_name" 
+                                    placeholder="Nome"
+                                />
+                                <input 
+                                    value={email} 
+                                    onChange={HandleSetEmail} 
+                                    required 
+                                    type="email" 
+                                    name="email" 
+                                    placeholder="Email"
+                                />
+                                <textarea
+                                value={message} 
+                                onChange={HandleSetMessage} 
+                                required 
+                                name="message" 
+                                placeholder="Comentário">
+                                </textarea>
+                                {isSendingForm ? 
+                                    <button disabled type="submit">Enviando</button> 
+                                    :
+                                    <button type="submit">Enviar</button>
+                                }
                             </form>
                         </div>
                     </div>
                 </div>
-            </div>        
+            </div>     
         </section>
         
     )
