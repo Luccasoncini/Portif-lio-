fabe407c12d58e166e7023972bd2127a6c2d7162
luccasoncini <luccadiassoncini@gmail.com>
2022-06-29 09:55:18 -0300

    add whatsapp icon

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 
 import './footer.scss'
 
@@ -12,6 +12,7 @@ export function Footer() {
                         <div className="redes-sociais-footer">
                             <a href="https://www.linkedin.com/in/lucca-soncini-727930207/" target="_blank"><FaLinkedinIn /></a>
                             <a href="https://github.com/Luccasoncini" target="_blank"><FaGithub /></a>
+                            <a href="https://api.whatsapp.com/send?1=pt_BR&phone=5511989785140"><FaWhatsapp /></a>
                         </div>  
                         <p>Feito com ♥ por Lucca Dias Soncini</p>
                     </div>
